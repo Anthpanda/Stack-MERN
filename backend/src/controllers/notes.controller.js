b3680511019ebb5e15d8564ef3baf8e753929fd2
Anthony <Anthony.otoniel@hotmail.com>
2021-05-11 19:14:30 -0400

    add note controller

--- backend/src/controllers/notes.controller.js
+++ backend/src/controllers/notes.controller.js
@@ -1,4 +1,29 @@
+export const getNotes = (req, res) => {
+    res.json({
+        message: 'Get - Users Routes'
+    });
+}
 
-export const getNotes =  (req, res) => {
- res.json('Users Routes');
+export const getNotesById = (req, res) => {
+    res.json({
+        message: 'Get - Users Routes by id'
+    });
+}
+
+export const createNotes = (req, res) => {
+    res.json({
+        message: 'Post - Users Routes'
+    });
+}
+
+export const deleteNotes = (req, res) => {
+    res.json({
+        message: 'Note - Deleted'
+    });
+}
+
+export const updateNotes = (req, res) => {
+    res.json({
+        message: 'Note -updated'
+    });
 }
--- backend/src/routes/notes.routes.js
+++ backend/src/routes/notes.routes.js
@@ -5,4 +5,12 @@ import * as notesCtrl from '../controllers/notes.controller'
 
 router.get('/', notesCtrl.getNotes);
 
+router.get('/:id', notesCtrl.getNotesById);
+
+router.post('/', notesCtrl.createNotes);
+
+router.put('/:id', notesCtrl.updateNotes);
+
+router.delete('/:id', notesCtrl.deleteNotes);
+
 export default router;
